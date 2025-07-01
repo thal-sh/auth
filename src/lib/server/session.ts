a25e6ecd4fb4f161f43c62761cea5bf9a0fa7013
thal-sh <thaleoussendaris@gmail.com>
2025-07-01 17:16:49 +0300

    add (session): added validate session token

--- src/lib/server/session.ts
+++ src/lib/server/session.ts
@@ -1,8 +1,54 @@
 import prisma from '$lib/prisma';
 import { sha256 } from '@oslojs/crypto/sha2';
 import { encodeBase32, encodeHexLowerCase } from '@oslojs/encoding';
-import type { Session } from '@prisma/client';
+import type { Session, User } from '@prisma/client';
 import type { RequestEvent } from '@sveltejs/kit';
+
+export async function validateSessionToken(token: string): Promise<{
+	session: Session | null;
+	user: User | null;
+}> {
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+
+	const session = await prisma.session.findUnique({
+		where: { id: sessionId },
+		include: {
+			user: true
+		}
+	});
+
+	if (!session) {
+		return { session: null, user: null };
+	}
+
+	const now = new Date();
+
+	if (now >= session.expiresAt) {
+		await prisma.session.delete({
+			where: { id: session.id }
+		});
+		return { session: null, user: null };
+	}
+
+	const fifteenDays = 1000 * 60 * 60 * 24 * 15;
+	const thirtyDays = 1000 * 60 * 60 * 24 * 30;
+
+	if (now.getTime() >= session.expiresAt.getTime() - fifteenDays) {
+		const newExpiry = new Date(now.getTime() + thirtyDays);
+		await prisma.session.update({
+			where: { id: session.id },
+			data: {
+				expiresAt: newExpiry
+			}
+		});
+		session.expiresAt = newExpiry;
+	}
+
+	return {
+		session,
+		user: session.user
+	};
+}
 
 export function setSessionTokenCookie(event: RequestEvent, token: string, expiresAt: Date): void {
 	event.cookies.set('session', token, {
